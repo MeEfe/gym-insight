9967dd07856708c24006807e260d91b4e7a44326
EfeErter <efdal.erter@gmail.com>
2025-01-27 20:53:27 +0100

    Main page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,6 +5,7 @@ import Header from "../Header/Header";
 import CustomerFeedbackCarousel from "../Carousel/Carousel";
 import Footer from "../Footer/Footer";
 import Logo from "../../assets/logo.svg";
+import ArrowDown from "../../assets/arrow-down.svg";
 
 export default function Hero() {
 	const alignCenter = { display: "flex", alignItems: "center" };
@@ -37,7 +38,7 @@ export default function Hero() {
 					style={{ height: `${scrollProgress}%` }}
 				/>
 			</div>
-			<Parallax ref={parallax} pages={3}>
+			<Parallax ref={parallax} pages={8}>
 				<ParallaxLayer
 					offset={0}
 					speed={0.5}
@@ -56,34 +57,155 @@ export default function Hero() {
 									and enhance your workout gym
 								</p>
 							</div>
-							<svg
+							<img
 								id={styles.arrowDown}
-								width="30px"
-								height="30px"
-								viewBox="0 0 24 24"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M12 4V20M12 20L8 16M12 20L16 16"
-									stroke="#c2e0ff"
-									strokeWidth="2"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-								/>
-							</svg>
+								src={ArrowDown}
+								alt="arrowDown"
+							/>
 						</div>
 					</div>
 				</ParallaxLayer>
 
-				<ParallaxLayer offset={1} speed={0.5}>
-					<div className={styles.content}>
-						<h1>Discover Page 2</h1>
-						<p>Here is some more content on the second page.</p>
+				<ParallaxLayer
+					sticky={{ start: 1, end: 6 }}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-start",
+					}}
+				>
+					<div className={styles.cardContainer}>
+						<h2 className={styles.storyTitle}>
+							What we have to offer
+						</h2>
 					</div>
 				</ParallaxLayer>
 
-				<ParallaxLayer offset={2} speed={0.5}>
+				<ParallaxLayer
+					offset={2}
+					speed={1}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-end",
+					}}
+				>
+					<div className={`${styles.cardContainer} ${styles.start}`}>
+						<h2
+							className={`${styles.storyTitle} ${styles.informationTitle}`}
+						>
+							#001 Efficiency
+						</h2>
+						<p className={styles.informationParagraph}>
+							Maximize your gym’s potential with real-time
+							tracking and data-driven insights. Our solution
+							helps gym owners optimize space, manage equipment
+							utilization, and ensure a seamless experience for
+							gym goers. Stay ahead with precise analytics that
+							enhance operational efficiency and member
+							satisfaction.
+						</p>
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={3}
+					speed={1}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-end",
+					}}
+				>
+					<div className={`${styles.cardContainer} ${styles.start}`}>
+						<h2
+							className={`${styles.storyTitle} ${styles.informationTitle}`}
+						>
+							#002 Smart Utilization
+						</h2>
+						<p className={styles.informationParagraph}>
+							Optimize your gym space like never before. Our
+							advanced tracking technology provides real-time data
+							on equipment and facility usage, helping you
+							identify peak hours, underutilized assets, and areas
+							for improvement. Maximize efficiency and enhance the
+							experience for both gym owners and members.
+						</p>
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={4}
+					speed={1}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-end",
+					}}
+				>
+					<div className={`${styles.cardContainer} ${styles.start}`}>
+						<h2
+							className={`${styles.storyTitle} ${styles.informationTitle}`}
+						>
+							#003 Empowered Decisions
+						</h2>
+						<p className={styles.informationParagraph}>
+							Data-driven insights at your fingertips. With our
+							intuitive dashboard, gym owners can make informed
+							financial decisions, adjust pricing dynamically, and
+							understand what equipment adds the most value to
+							their business. Take control and stay ahead in a
+							competitive fitness market.
+						</p>
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={5}
+					speed={1}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-end",
+					}}
+				>
+					<div className={`${styles.cardContainer} ${styles.start}`}>
+						<h2
+							className={`${styles.storyTitle} ${styles.informationTitle}`}
+						>
+							#004 Seamless Experience
+						</h2>
+						<p className={styles.informationParagraph}>
+							For gym-goers, our app provides a hassle-free way to
+							plan their workouts. Check equipment availability,
+							monitor gym capacity, and avoid the rush with live
+							updates. Your fitness journey just got smarter, more
+							efficient, and stress-free.
+						</p>
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer
+					offset={6}
+					speed={1}
+					style={{
+						...alignCenter,
+						justifyContent: "flex-end",
+					}}
+				>
+					<div className={`${styles.cardContainer} ${styles.start}`}>
+						<h2
+							className={`${styles.storyTitle} ${styles.informationTitle}`}
+						>
+							#005 Future of Fitness
+						</h2>
+						<p className={styles.informationParagraph}>
+							We’re here to revolutionize the gym experience for
+							both owners and members. By combining real-time data
+							with expert consultation, we help gyms stay
+							future-ready with actionable strategies that drive
+							growth, profitability, and satisfaction. Welcome to
+							the future of fitness management.
+						</p>
+					</div>
+				</ParallaxLayer>
+
+				<ParallaxLayer offset={7} speed={0.5}>
 					<div className={styles.pageEndContainer}>
 						<CustomerFeedbackCarousel />
 						<Footer />
